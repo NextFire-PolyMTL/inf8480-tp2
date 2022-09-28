5a5a902eee73dcc7bccc06efd5e1576f864170a0
NextFire <git@yuru.moe>
2022-09-28 14:52:50 +0000

    formatting

--- task_scheduler_correction/manager.cc
+++ task_scheduler_correction/manager.cc
@@ -9,43 +9,47 @@
 
 #include "lttng-traces/grpc_tracing.h"
 
-
 using grpc::Channel;
 using grpc::ChannelArguments;
 using grpc::ClientContext;
 using grpc::Status;
 using operation::Operation;
-using operation::OperationRequest;
 using operation::OperationReply;
+using operation::OperationRequest;
 
-class OperationManager {
+class OperationManager
+{
 public:
-    OperationManager(std::shared_ptr <Channel> channel)
-            : stub_(Operation::NewStub(channel)) {}
+    OperationManager(std::shared_ptr<Channel> channel)
+        : stub_(Operation::NewStub(channel)) {}
 
     // Assembles the client's payload, sends it and presents the response back from the server
-    std::string ProcessOperation(const std::string &task) {
+    std::string ProcessOperation(const std::string &task)
+    {
         char cstr1[task.size() + 1];
         strcpy(cstr1, task.c_str());
-        tracepoint(grpc_tracing, manager_send, cstr1);//DO NOT MODIFY
+        tracepoint(grpc_tracing, manager_send, cstr1); // DO NOT MODIFY
         OperationRequest request;
-        request.set_task(task);// Data we are sending to the server.
-        OperationReply reply; // Container for the data we expect from the server.
+        request.set_task(task); // Data we are sending to the server.
+        OperationReply reply;   // Container for the data we expect from the server.
 
         // Context for the client. It could be used to convey extra information to
         // the server and/or tweak certain RPC behaviors.
         ClientContext context;
 
-//TODO : send the gRPC call
+        // TODO: send the gRPC call
         Status status = stub_->ProcessOperation(&context, request, &reply);
 
         char cstr2[reply.message().size() + 1];
         strcpy(cstr2, reply.message().c_str());
-        tracepoint(grpc_tracing, manager_recv, cstr2);//DO NOT MODIFY
+        tracepoint(grpc_tracing, manager_recv, cstr2); // DO NOT MODIFY
         // Act upon its status.
-        if (status.ok()) {
+        if (status.ok())
+        {
             return reply.message();
-        } else {
+        }
+        else
+        {
             std::cout << status.error_code() << ": " << status.error_message()
                       << std::endl;
             return 0;
@@ -53,23 +57,22 @@ public:
     }
 
 private:
-    //TODO : make something for gRPC
-    std::unique_ptr <Operation::Stub> stub_;
+    // TODO: make something for gRPC
+    std::unique_ptr<Operation::Stub> stub_;
 };
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     // Instantiate the client. It requires a channel, out of which the actual RPCs  are created.
-    //This channel models a connection to an endpoint
+    // This channel models a connection to an endpoint
     ChannelArguments args;
     // Set the load balancing policy for the channel.
     args.SetLoadBalancingPolicyName("round_robin");
 
-
     OperationManager manager(grpc::CreateCustomChannel(
-            "localhost:50051", grpc::InsecureChannelCredentials(), args));
+        "localhost:50051", grpc::InsecureChannelCredentials(), args));
 
     std::string task("tache envoyee");
-
 
     std::string reply = manager.ProcessOperation(task);
     std::cout << "Reply from server : " << reply << std::endl;
